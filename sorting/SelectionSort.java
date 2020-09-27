81b65a975435c1485062207053d71b8a7f00aa08
Naresh Goty <gnr.springs@gmail.com>
2020-09-26 22:12:17 -0700

    Update SelectionSort.java

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,22 +2,22 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
-  public static void sort(int[] numbers){
+  public static void sort(Comparable[] items){
     int sortedIndex = -1;
     
-    while(sortedIndex < numbers.length-1) {
+    while(sortedIndex < items.length-1) {
         int index = sortedIndex +1;
-        for (int i = index + 1; i < numbers.length; i++) {
-          if (numbers[index] > numbers[i]) {
+        for (int i = index + 1; i < items.length; i++) {
+          if (items[index].compareTo(items[i]) > 0) {
               index = i;
           }
         }
         
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(items));
         //swap
-        int temp = numbers[index];
-        numbers[index] = numbers[sortedIndex + 1];
-        numbers[sortedIndex + 1] = temp;
+        Comparable temp = items[index];
+        items[index] = items[sortedIndex + 1];
+        items[sortedIndex + 1] = temp;
         
         sortedIndex++;
         
@@ -25,7 +25,7 @@ public class SelectionSort {
   }
   
   public static void main(String[] args){
-    int[] numbers = {2,5,8, 1, 20, 4, 6};
+    Integer[] numbers = {2,5,8, 1, 20, 4, 6};
     sort(numbers);
     System.out.println(Arrays.toString(numbers));
   }
